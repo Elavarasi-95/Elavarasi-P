c666aec6e30d3123bd637119b7cf040eefe88304
Elavarasi-95 <elavarasipinnai01@gmail.com>
2026-02-06 12:53:32 +0300

    saba code

--- src/test/java/org/pack/Sample.java
+++ src/test/java/org/pack/Sample.java
@@ -5,8 +5,9 @@ import org.junit.Test;
 public class Sample {
 	@Test
 	public void test() {
-		// TODO Auto-generated method stub
-
+	System.out.println("Test1");
+	System.out.println("Test2");
+	System.out.println("Test3");
 	}
 
 }
