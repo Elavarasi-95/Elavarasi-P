fb2fb81914d6c1a77cb2c07dc801d024bfd14dc3
Elavarasi-95 <elavarasipinnai01@gmail.com>
2026-02-06 13:03:17 +0300

    saba code

--- src/test/java/org/pack/Sample.java
+++ src/test/java/org/pack/Sample.java
@@ -5,9 +5,9 @@ import org.junit.Test;
 public class Sample {
 	@Test
 	public void test() {
-	System.out.println("Test1");
-	System.out.println("Test2");
-	System.out.println("Test3");
+	System.out.println("Test4");
+	System.out.println("Test5");
+	System.out.println("Test6");
 	}
 
 }
